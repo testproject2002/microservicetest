4d90c90551f92a76237ed79d2cc202db5ac668e0
kiran <kirankiranhalepujari@gmail.com>
2024-06-14 13:24:03 +0530

    added files

---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+// src/components/Navbar.js
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          YourLogo <i className="fab fa-typo3" />
+        </Link>
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <Link to="/" className="nav-links">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/login" className="nav-links">
+              Login
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/register" className="nav-links">
+              Register
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/formsubmit" className="nav-links">
+              Form Submit
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
